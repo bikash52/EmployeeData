4c37e71e2f8628494d534cd1f5539939ba2ea12e
Your Name <jbikashkumar58@gmail.com>
2025-11-08 15:18:00 +0530

    Updated Code

--- src/main/java/com/emp/repository/EmployeeRepository.java
+++ src/main/java/com/emp/repository/EmployeeRepository.java
@@ -1,9 +1,17 @@
 package com.emp.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.emp.model.Employee;
 
-public interface EmployeeRepository extends CrudRepository<Employee, Long>{
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+	Optional<Employee> findByEmail(String email);
 
+	Optional<Employee> findByMobile(String mobile);
+
+	Iterable<Employee> findByDepartment(String department);
+
+	Iterable<Employee> findByEmailOrMobile(String email, String mobile);
 }
--- src/test/java/com/emp/EmployeeManagementApplicationTests.java
+++ src/test/java/com/emp/EmployeeManagementApplicationTests.java
@@ -53,10 +53,76 @@ class EmployeeManagementApplicationTests {
 			System.out.println("No record found");
 		}
 	}
-	
+
 	@Test
 	void deleteEmp() {
 		employeeRepository.deleteById(4L);
 	}
 
+	@Test
+	void getAllEmp() {
+		Iterable<Employee> all = employeeRepository.findAll();
+		for (Employee e : all) {
+			System.out.println(e.getId());
+			System.out.println(e.getName());
+			System.out.println(e.getEmail());
+			System.out.println(e.getMobile());
+			System.out.println(e.getDepartment());
+			System.out.println(e.getSalary());
+		}
+	}
+
+	@Test
+	void getByEmail() {
+		Optional<Employee> byEmail = employeeRepository.findByEmail("rakesh@gmail.com");
+		if (byEmail.isPresent()) {
+			Employee email = byEmail.get();
+			System.out.println(email.getId());
+			System.out.println(email.getName());
+			System.out.println(email.getEmail());
+			System.out.println(email.getMobile());
+			System.out.println(email.getDepartment());
+			System.out.println(email.getSalary());
+		}
+	}
+
+	@Test
+	void getByMobile() {
+		Optional<Employee> byMobile = employeeRepository.findByMobile("6366487924");
+		if (byMobile.isPresent()) {
+			Employee mobile = byMobile.get();
+			System.out.println(mobile.getId());
+			System.out.println(mobile.getName());
+			System.out.println(mobile.getEmail());
+			System.out.println(mobile.getMobile());
+			System.out.println(mobile.getDepartment());
+			System.out.println(mobile.getSalary());
+		}
+	}
+
+	@Test
+	void getByDepartment() {
+		Iterable<Employee> dep = employeeRepository.findByDepartment("IT");
+		for (Employee d : dep) {
+			System.out.println(d.getId());
+			System.out.println(d.getName());
+			System.out.println(d.getEmail());
+			System.out.println(d.getMobile());
+			System.out.println(d.getDepartment());
+			System.out.println(d.getSalary());
+		}
+	}
+
+	@Test
+	void getByEmailOrMobile() {
+		Iterable<Employee> byEmailOrMobile = employeeRepository.findByEmailOrMobile("rakesh@gmail.com", "6366432873");
+		for (Employee e : byEmailOrMobile) {
+			System.out.println(e.getId());
+			System.out.println(e.getName());
+			System.out.println(e.getEmail());
+			System.out.println(e.getMobile());
+			System.out.println(e.getDepartment());
+			System.out.println(e.getSalary());
+		}
+	}
 }
